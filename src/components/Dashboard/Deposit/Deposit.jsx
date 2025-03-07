bd29b57b4766f5c36a012e400b1a47c3a420b618
TuanNghia295 <tuannghiait2905@gmail.com>
2025-03-07 15:42:52 +0700

    Xong lịch sử nạp của admin và sale

---
+++ src/components/Dashboard/Deposit/Deposit.jsx
@@ -0,0 +1,161 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+import { useDeposit } from '../../../services/depositService';
+import HistoryIcon from '@mui/icons-material/History';
+import { Outlet, useNavigate, useLocation } from 'react-router';
+
+export default function Deposit() {
+  const [deposits, setDeposits] = useState([]);
+  const [page, setPage] = useState(1);
+  const [desiredPage, setDesiredPage] = useState(1);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {
+    isLoadingListDeposit,
+    listDeposits,
+    totalPage,
+    acceptDepositMutation,
+    isLoadingAcceptDeposit,
+    rejectDepositMutation,
+    isLoadingRejectDeposit,
+  } = useDeposit({
+    limit: 8,
+    page: desiredPage,
+  });
+
+  const handleAccept = (depositId) => {
+    acceptDepositMutation(depositId);
+    if (isLoadingAcceptDeposit) return;
+  };
+
+  const handleReject = (depositId) => {
+    rejectDepositMutation(depositId);
+    if (isLoadingRejectDeposit) return;
+  };
+
+  const getStatusText = (status) => {
+    switch (status) {
+      case 'PENDING':
+        return 'Đang chờ';
+      case 'ACCEPTED':
+        return 'Đã duyệt';
+      case 'REJECTED':
+        return 'Đã từ chối';
+      default:
+        return status;
+    }
+  };
+
+  // chuyển trang
+  const handleNextPage = () => {
+    setDesiredPage((prev) => prev + 1);
+  };
+  const handlePreviousPage = () => {
+    setDesiredPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const formatNumber = (value) => {
+    if (typeof value !== 'string' && typeof value !== 'number') {
+      return '';
+    }
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  };
+
+  useEffect(() => {
+    if (!isLoadingListDeposit) {
+      setDeposits(listDeposits);
+      setPage(desiredPage);
+    }
+  }, [isLoadingListDeposit, listDeposits, desiredPage]);
+
+  return (
+    <div className="p-4 bg-white rounded-md shadow-md">
+      {location.pathname === '/dashboard/deposit' && (
+        <>
+          <div className="flex justify-start items-center text-2xl font-bold mb-4">
+            <h1 className="flex-1">Quản lý nạp điểm</h1>
+            <Button
+              variant="outlined"
+              color="secondary"
+              startIcon={<HistoryIcon />}
+              onClick={() => navigate('history')}
+            >
+              Lịch sử
+            </Button>
+          </div>
+          <table className="w-full border-collapse border border-gray-300 mt-4">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 p-2">Người dùng</th>
+                <th className="border border-gray-300 p-2">Số điểm</th>
+                <th className="border border-gray-300 p-2">Trạng thái</th>
+                <th className="border border-gray-300 p-2"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {deposits?.map((deposit) => (
+                <tr key={deposit.id}>
+                  <td className="border border-gray-300 p-2 text-center">
+                    {deposit?.user?.fullName}
+                  </td>
+                  <td className="border border-gray-300 p-2 text-center">
+                    {formatNumber(deposit?.amount)}
+                  </td>
+                  <td className="border border-gray-300 p-2 text-center">
+                    {getStatusText(deposit?.status)}
+                  </td>
+                  <td className="border border-gray-300 p-2 text-center flex justify-around">
+                    <Button
+                      variant="text"
+                      color="primary"
+                      onClick={() => handleAccept(deposit?.id)}
+                      disabled={
+                        deposit?.status === 'ACCEPTED' ||
+                        deposit?.status === 'REJECTED'
+                      }
+                    >
+                      Chấp nhận
+                    </Button>
+                    <Button
+                      variant="text"
+                      color="error"
+                      onClick={() => handleReject(deposit?.id)}
+                      disabled={
+                        deposit?.status === 'ACCEPTED' ||
+                        deposit?.status === 'REJECTED'
+                      }
+                    >
+                      Từ chối
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-between mt-4 text-center items-center">
+            <Button
+              variant="text"
+              color="warning"
+              onClick={handlePreviousPage}
+              disabled={page === 1}
+            >
+              Trang trước
+            </Button>
+            <span>
+              {page}/{totalPage}
+            </span>
+            <Button
+              variant="text"
+              color="warning"
+              onClick={handleNextPage}
+              disabled={page === totalPage}
+            >
+              Trang sau
+            </Button>
+          </div>
+        </>
+      )}
+      <Outlet />
+    </div>
+  );
+}
---
+++ src/components/Dashboard/Deposit/DepositHistory.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import {
+  Button,
+  TablePagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useDepositHistory } from '../../../services/history/depositHistoryService';
+
+const formatMoney = (money) => {
+  return new Intl.NumberFormat('vi-VN').format(money);
+};
+
+const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleDateString('vi-VN');
+};
+
+const getRoleDisplayName = (role) => {
+  return role === 'MANAGER' ? 'ADMIN' : role;
+};
+
+export default function DepositHistory() {
+  const navigate = useNavigate();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(1);
+
+  const { depositHistory, pagination, totalPage } = useDepositHistory({
+    limit: rowsPerPage,
+    page: page + 1,
+    q: '',
+  });
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <div>
+      <div className="flex justify-start items-center mb-4 border-b pb-4">
+        <div className="flex-1">
+          <Button startIcon={<ArrowBackIcon />} onClick={() => navigate(-1)} />
+        </div>
+        <h1 className="font-[500] text-primary text-xl">Lịch sử nạp</h1>
+      </div>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Người nạp</TableCell>
+            <TableCell>Chức Vụ</TableCell>
+            <TableCell>Số tiền</TableCell>
+            <TableCell>Người dùng</TableCell>
+            <TableCell>Ngày nạp</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {depositHistory?.map((deposit) => (
+            <TableRow key={deposit.id}>
+              <TableCell>
+                {getRoleDisplayName(deposit?.manager?.role) === 'ADMIN'
+                  ? 'ADMIN'
+                  : deposit.manager?.fullName}
+              </TableCell>
+              <TableCell>
+                {getRoleDisplayName(deposit?.manager?.role)}
+              </TableCell>
+              <TableCell>{formatMoney(deposit.amount)}</TableCell>
+              <TableCell>{deposit?.user?.fullName}</TableCell>
+              <TableCell>{formatDate(deposit?.createdAt)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <TablePagination
+        component="div"
+        count={totalPage}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </div>
+  );
+}
